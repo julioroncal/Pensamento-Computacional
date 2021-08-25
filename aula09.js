f3ee33431a5d77205eccd57142724c957ed5c472
JULIO JOEL RONCAL AGUILAR <julioroncal.lds@gmail.com>
2021-08-25 20:39:25 -0300

    Commit Aula09
    Prática de Estruturas Condicionais
    Projeto Microondas

---
+++ aula09.js
@@ -0,0 +1,7 @@
+//1 - Escreva uma função utilizando uma estrutura básica de IF/ELSE que verifique uma senha. A senha válida é o número 1234. Devem ser impressas as seguintes mensagens:
+//	- ACESSO PERMITIDO caso a senha seja válida.
+//	- ACESSO NEGADO caso a senha seja inválida.
+
+function verifica(){
+    
+}
